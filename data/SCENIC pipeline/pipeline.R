e457bf23ed38ca8ffb97a2998912268110cf2c64
mapoUNMC <mapostol@unmc.edu>
2025-07-10 16:38:19 -0500

    test

--- data/SCENIC pipeline/pipeline.R
+++ data/SCENIC pipeline/pipeline.R
@@ -36,7 +36,7 @@ save(pbmc, file = "pbmc_new.RData")
 ### labeling done in post R-session (Jul10)
 ## same biomarkers found in PBMC3k tutorial: https://satijalab.org/seurat/articles/pbmc3k_tutorial
 ## new.cluster.ids <- c("Naive CD4 T", "CD14+ Mono", "Memory CD4 T", "B", "CD8 T", "FCGR3A+ Mono",
-    "NK", "DC", "Platelet")
+##    "NK", "DC", "Platelet")
 ## names(new.cluster.ids) <- levels(pbmc)
 ## pbmc <- RenameIdents(pbmc, new.cluster.ids)
 ## DimPlot(pbmc, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
